e235b6b0b377172bbacd20f45b07d2fcda0d4848
Tenzin Kalden <36243752+tkalden@users.noreply.github.com>
2022-11-07 17:23:56 -0500

    Revert "Added user login, sign up and log out"

--- bigQuery.py
+++ bigQuery.py
@@ -5,8 +5,8 @@ logging.basicConfig(level=logging.INFO)
 
 client = bigquery.Client()
 
+
 def write_to_bigquery(df,table_id):
-    logging.info('Saving data')
     client.load_table_from_dataframe(df, table_id)
 
 
@@ -40,23 +40,6 @@ def get_portfolios_by_user_id(user_id):
     WHERE t.user_id = @user_id
      """
     query_parameters = [bigquery.ScalarQueryParameter("user_id", "STRING", user_id)]
-    return run_query(query,query_parameters)
-
-def get_user_by_id_and_pw(user_id,password):
-    query = """
-    SELECT * FROM `stockdataextractor.stock.user-table` t
-    WHERE t.user_id = @user_id
-    AND t.password = @password
-     """
-    query_parameters = [bigquery.ScalarQueryParameter("user_id", "STRING", user_id),bigquery.ScalarQueryParameter("password", "STRING", password)]
-    return run_query(query,query_parameters)
-
-def get_user_by_id(email):
-    query = """
-    SELECT * FROM `stockdataextractor.stock.user-table` t
-    WHERE t.email = @email
-     """
-    query_parameters = [bigquery.ScalarQueryParameter("email", "STRING", email)]
     return run_query(query,query_parameters)
 
 def get_portfolio_by_id(portfolio_id):
@@ -100,11 +83,3 @@ def get_query(index,sector):
             ORDER BY Ticker DESC
             LIMIT 1000 """
     return query
-
-def insert_rows(table_id,rows_to_insert):
-    table = client.get_table(table_id)
-    errors = client.insert_rows_json(table, rows_to_insert)  # Make an API request.
-    if errors == []:
-        logging.debug("New rows have been added.")
-    else:
-        logging.error("Encountered errors while inserting rows: {}".format(errors))
