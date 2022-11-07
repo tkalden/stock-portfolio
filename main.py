5caf980a32afd4c84ab1855ab376411f659a1ff4
Tenzin Kalden <36243752+tkalden@users.noreply.github.com>
2022-11-07 17:23:56 -0500

    Revert "Added user login, sign up and log out"

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
-from flask import Blueprint
+from math import comb
 import stockValueExtract
 import helper
-from flask import render_template, request
+from flask import Flask, render_template, request, session,redirect,url_for
+from flask_toastr import Toastr
 import pandas as pd
-from __init__ import create_app
-from flask_login import current_user
-import logging
+import numpy as np
+import os
 
-main = Blueprint('main', __name__)
+toastr = Toastr()
 
+config = {
+    "SECRET_KEY": "e0af172472bfdc4bc8292763f86e3abe0e2eb9a8cf68d12f"
+}
+
+app = Flask(__name__)
+app.config.from_mapping(config)
+
+toastr.init_app(app)
 
 stockValueExtractor = stockValueExtract.stock()
 
-@main.route('/', methods=['POST','GET'])
+@app.route('/', methods=['POST','GET'])
 def home():
    stockValueExtractor.cache_stock_data()
-   return render_template('home.html')
+   return render_template('home.html', title= "Welcome StockNity Members!")
 
-@main.route('/profile', methods=['POST','GET'])
-def profile():
-   stockValueExtractor.cache_stock_data()
-   return render_template('profile.html',name = current_user.name)
+@app.route('/login', methods=['POST','GET'])
+def login():
+   #session['user_id'] = 'albertkalden@gmail.com'
+   return 'LOGIN COMING SOON'
 
-@main.route('/screener', methods=["GET","POST"])
+@app.route('/screener', methods=["GET","POST"])
 def screener():
  index = 'S&P 500'
  sector = ''
@@ -35,8 +43,11 @@ def screener():
  return render_template('screener.html', index_sector_lists = helper.index_sector(), title = "{index} {sector} Data".format(sector = sector, index =index)
  )
 
-@main.route('/portfolio', methods=["GET","POST"])
+@app.route('/portfolio', methods=["GET","POST"])
 def build():
+    user_id = ''
+    if session.get('user_id') is not None:
+         user_id = session['user_id']
     portfolio = pd.DataFrame()
     stock_data = pd.read_pickle(helper.get_pickle_file()["stock"])
     ticker_list = sorted(list(set(stock_data["Ticker"])))
@@ -44,26 +55,38 @@ def build():
     total_portfolio_risk = 0
     if request.method == 'POST':
         if request.form["btn"]=="Build":
-            strength_df = stockValueExtractor.update_strength_data(sector=request.form.get('sector'), index = request.form.get('index'),stock_type = request.form.get('stock_type'))
-            portfolio = stockValueExtractor.build_portfolio_with_top_stocks(strength_df,request.form.get('investing_amount'),request.form.get('max_stock_price'))
+            sector = request.form.get('sector')
+            index = request.form.get('index')
+            stock_type = request.form.get('stock_type')
+            investing_amount = request.form.get('investing_amount')
+            maximum_stock_price = request.form.get('max_stock_price')
+            strength_df = stockValueExtractor.update_strength_data(sector=sector, index = index,stock_type = stock_type)
+            portfolio = stockValueExtractor.build_portfolio_with_top_stocks(strength_df,investing_amount,maximum_stock_price)
         elif request.form["btn"]=="Optimize":
-            logging.info("Extracting form data")
-            strength_df = stockValueExtractor.update_strength_data(sector = 'Any', index = 'S&P 500',stock_type = request.form["stock_type"])       
-            portfolio = stockValueExtractor.build_portfolio_from_user_input_tickers(strength_df,(list(set(request.form.getlist("stock[]")))), request.form["threshold"],request.form["expected_return_value"], request.form["investing_amount"],request.form.get('max_stock_price'))
+            app.logger.info("Extracting form data")
+            threshold = request.form["threshold"]
+            stock_type = request.form["stock_type"]
+            investing_amount = request.form["investing_amount"]
+            maximum_stock_price = request.form.get('max_stock_price')
+            expected_return_value = request.form["expected_return_value"]
+            selected_ticker_list = (list(set(request.form.getlist("stock[]"))))
+            strength_df = stockValueExtractor.update_strength_data(sector = 'Any', index = 'S&P 500',stock_type = stock_type)       
+            portfolio = stockValueExtractor.build_portfolio_from_user_input_tickers(strength_df,selected_ticker_list, threshold,expected_return_value, investing_amount,maximum_stock_price)
         elif request.form["btn"]=="Save Portfolio":
              # for some reason the portfolio varible goes to empty dataframe when the save is clicked.Following
             # is the work around
              portfolio = pd.read_pickle(helper.get_pickle_file()["portfolio"])
              if not portfolio.empty:
                 app.logger.info(f'Saving portfolio Data %',portfolio)
-                stockValueExtractor.save_portfolio_data(portfolio,current_user.id)
-        total_portfolio_return = round(stockValueExtractor.calculate_portfolio_return(portfolio),2)
-        total_portfolio_risk = round(stockValueExtractor.calculate_portfolio_risk(portfolio),2)
+                stockValueExtractor.save_portfolio_data(portfolio,session['user_id'])
+                stockValueExtractor.save_user_data(session['user_id'])
+        total_portfolio_return = stockValueExtractor.calculate_portfolio_return(portfolio)
+        total_portfolio_risk = stockValueExtractor.calculate_portfolio_risk(portfolio)
     stockValueExtractor.pickle_file(portfolio,'portfolio')  
     title = "Portfolio Return: {portfolio_return} % | Portfolio Risk: {risk} %".format(portfolio_return = total_portfolio_return, risk = total_portfolio_risk)
-    return render_template('buildPortfolio.html',stock_types = helper.get_stock_type(),ticker_list = ticker_list,parameters=helper.get_optimization_parameters(),ticker_sector_lists=helper.index_select_attributes(),title= title,columns = helper.build_porfolio_column())
+    return render_template('buildPortfolio.html',stock_types = helper.get_stock_type(),ticker_list = ticker_list,parameters=helper.get_optimization_parameters(),ticker_sector_lists=helper.index_select_attributes(),title= title,columns = helper.build_porfolio_column(), user_id = user_id)
 
-@main.route('/screener/data')
+@app.route('/screener/data')
 def stock_data():  
     #by default if there is no screener data at all then populate with defualt stock data
     if not stockValueExtractor.checkFile('screener'):
@@ -72,36 +95,38 @@ def stock_data():
         data = pd.read_pickle(helper.get_pickle_file()["screener"]).to_dict('records')
     return {'data': data}
 
-@main.route('/my-portfolio', methods=["GET","POST"])
+@app.route('/my-portfolio', methods=["GET","POST"])
 def portfolio():
-    return render_template('portfolio.html', columns = helper.build_porfolio_column())
+    user_id = ''
+    if session.get('user_id') is not None:
+         user_id = session['user_id']
+    return render_template('portfolio.html',user_id = user_id, columns = helper.build_porfolio_column())
 
-@main.route('/my-portfolio/data')
+@app.route('/my-portfolio/data')
 def portfolio_data():
-    return {'data': stockValueExtractor.get_portfolios_by_user_id(current_user.id).to_dict('records')}
+    return {'data': stockValueExtractor.get_portfolios_by_user_id(session['user_id']).to_dict('records')}
 
-@main.route('/portfolio/data')
+@app.route('/portfolio/data')
 def build_portfolio_data():
     return {'data': pd.read_pickle(helper.get_pickle_file()["portfolio"]).to_dict('records')}
 
-@main.route('/value-chart')
+@app.route('/value-chart')
 def valueChart():
     stockValueExtractor = stockValueExtract.stock()
     charts = stockValueExtractor.get_chart_data("value",helper.StockType.VALUE.value,helper.Metric.STRENGTH.value,overwrite=False)
     return render_template('chart.html',charts = charts,title= "Top Value Stocks" )
 
-@main.route('/growth-chart')
+@app.route('/growth-chart')
 def growthChart():
     stockValueExtractor = stockValueExtract.stock()
     charts = stockValueExtractor.get_chart_data("growth", helper.StockType.GROWTH.value, helper.Metric.STRENGTH.value,overwrite=False)
     return render_template('chart.html',charts = charts, title= "Top Growth Stocks")
 
-@main.route('/dividend-chart')
+@app.route('/dividend-chart')
 def dividendChart():
     stockValueExtractor = stockValueExtract.stock()
     charts = stockValueExtractor.get_chart_data("dividend", helper.StockType.NONE.value, helper.Metric.DIVIDEND.value,overwrite=False)
     return render_template('chart.html',charts = charts, title= "Top Dividend Stocks" )
 
-app = create_app() # we initialize our flask app using the __init__.py function
-if __name__ == '__main__':
-    app.run(debug=True) # run the flask app on debug mode
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
