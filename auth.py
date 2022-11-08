91805369881404a05fd2f6fd616eafe62c3d2392
Tenzin Kalden <albertkalden@gmail.com.com>
2022-11-07 20:55:35 -0500

    handled the user index out of error and put the flash warning in the log in or signup box

--- auth.py
+++ auth.py
@@ -10,23 +10,24 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST']) # define login page path
 def login(): # define login page fucntion
+    error = ''
     if request.method=='GET': # if the request is a GET we return the login page
         return render_template('login.html')
     else: # if the request is POST the we check if the user exist and with te right password
         email = request.form.get('email')
         password = request.form.get('password')
         remember = True if request.form.get('remember') else False
-        user = User.updateUserData(email)[0]
+        user = User.updateUserData(email)
         # check if the user actually exists
         # take the user-supplied password, hash it, and compare it to the hashed password in the database
         if not user:
-            flash('Please sign up before!','error')
+            flash('Please sign up before!')
             return redirect(url_for('auth.login'))
-        elif not check_password_hash(user.password, password):
-            flash('Please check your login details and try again.','error')
+        elif not check_password_hash(user[0].password, password):
+            flash('Please check your login details and try again.')
             return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
         # if the above check passes, then we know the user has the right credentials
-        login_user(user, remember=remember)
+        login_user(user[0], remember=remember)
         flash('Login Successful','success')
         return redirect(url_for('main.profile'))
 
@@ -39,12 +40,14 @@ def signup(): # define the sign up function
         name = request.form.get('name')
         password = request.form.get('password')
         confirm = request.form.get('confirm')
-        user = User.get(email)
+        user = User.updateUserData(email)
+        print('user',user)
         if user: # if a user is found, we want to redirect back to signup page so user can try again
-            flash('Email address already exists','error')
+            flash('Email address already exists.Please try loggin in')
             return redirect(url_for('auth.signup'))
         elif password!= confirm:
             flash('Passwords didnot match','error')
+            return redirect(url_for('auth.signup'))
         # create a new user with the form data. Hash the password so the plaintext version isn't saved.
         UserCRUD.save_user_data(email=email, name=name, password=generate_password_hash(password, method='sha256')) #
         return redirect(url_for('auth.login'))
