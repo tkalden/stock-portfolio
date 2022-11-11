4e73d0f479d3f381a0c1539f79b3d0a2d4e27a65
Tenzin Kalden <albertkalden@gmail.com.com>
2022-11-11 13:32:23 -0500

    added subscription functionality

--- utilities/userFunction.py
+++ utilities/userFunction.py
@@ -1,17 +1,40 @@
 import logging
 import utilities.bigQuery as bigQuery
 import uuid
+import utilities.constant as constant
+from flask import flash
 
 logging.basicConfig(level = logging.INFO)
+constant = constant.load_constant()
+table_names = constant["table_names"]
 
 class UserCRUD():
-    def get_user_by_email(email):
+    def get_user_by_email(self,email):
         return bigQuery.get_user_by_id(email)
 
-    def save_user_data(email,name,password):
+    def save_user_data(self,email,name,password):
         uid = uuid.uuid4()
         rows_to_insert = [
         {u"email": email, u"name": name, u"password": password, u"id": uid.hex}]
-        bigQuery.insert_rows('stockdataextractor.stock.user-table',rows_to_insert)
+        bigQuery.insert_rows(table_names["user"],rows_to_insert)
+    
+    def subscribe(self,email):
+        rows_to_insert = [
+        {u"email": email}]
+        bigQuery.insert_rows(table_names["subscription"],rows_to_insert)
+    
+    def get_subscription_by_id(self,email):
+        return bigQuery.get_subscription_by_id(email)
+    
+    def subscribe(self,subscribe_email):
+        if  self.get_subscription_by_id(subscribe_email).empty:
+            self.subscribe(subscribe_email)
+            flash('Thanks for the subscription','success')
+        else:
+            flash('This email is already subscribed.','warning')
+
+    
+
+
 
     
