16c3bf357255e8ae3674ac28572323437a928323
Tenzin Kalden <albertkalden@gmail.com.com>
2023-11-14 20:14:26 -0500

    added logs

--- methods/news.py
+++ methods/news.py
@@ -1,12 +1,13 @@
 from finvizfinance.news import News
-import requests
-from bs4 import BeautifulSoup
+import logging
+
 
 def get_news():
+    logging.info("Getting news");
     news = News()
     news = news.get_news()['news']
+    logging.info("News %s",news.head(5))
     return news
-
 
 
             
